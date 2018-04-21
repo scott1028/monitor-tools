fe9f4228eef0843a6de3c98e564e49a0e0f0902f
scott <scott.lan@taisys.com>
2018-04-22 00:14:23 +0800

    finish navbar sample

--- src/App.js
+++ src/App.js
@@ -12,18 +12,65 @@ import {
 } from 'react-router-dom'
 
 class App extends Component {
+  componentDidMount(){
+    document.addEventListener('DOMContentLoaded', function () {
+      // Get all "navbar-burger" elements
+      var $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll('.navbar-burger'), 0);
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+          $el.addEventListener('click', function () {
+
+            // Get the target from the "data-target" attribute
+            var target = $el.dataset.target;
+            var $target = document.getElementById(target);
+
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+          });
+        });
+      }
+    });
+  }
   render() {
     return (
       <Router>
         <div className="container">
-          <div className="navbar-menu">
-            <div class="navbar-start">
-              <Link className="navbar-item" to="/">ServerMonitorPage</Link>
-              <Link className="navbar-item" to="/ReleaseControlPage">ReleaseControlPage</Link>
+          <nav className="navbar">
+            <div className="navbar-brand">
+              <a className="navbar-item"
+                 href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png"
+                     alt="Bulma: a modern CSS framework based on Flexbox"
+                     width="112"
+                     height="28" />
+              </a>
+              <div className="navbar-burger burger"
+                   data-target="navbarTop">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
             </div>
-          </div>
-          <Route exact path="/" component={ServerMonitorPage}/>
-          <Route path="/ReleaseControlPage" component={ReleaseControlPage}/>
+
+            <div id="navbarTop"
+                 className="navbar-menu">
+              <div className="navbar-start">
+                <Link className="navbar-item"
+                      to="/">ServerMonitorPage</Link>
+                <Link className="navbar-item"
+                      to="/ReleaseControlPage">ReleaseControlPage</Link>
+              </div>
+            </div>
+          </nav>
+          <Route exact path="/" component={ServerMonitorPage} />
+          <Route path="/ReleaseControlPage" component={ReleaseControlPage} />
         </div>
       </Router>
     );
