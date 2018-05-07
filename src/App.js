2ce643bc77e1ae734ecb80093f0e593adb57dfc4
scott <mic1028002@gmail.com>
2018-05-07 22:54:35 +0800

    update code

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import {
   Link
 } from 'react-router-dom'
 
-import RounteCmp from './RouteCmp';  // testcase-2
-
 const DEFAULT = {
   value: 1,
 };
@@ -46,12 +44,13 @@ class App extends Component {
     });
   }
   render() {
+    console.log(1111);
     return (
       <Router>
         {
-          this.props.version % 2 === 0
+          this.props.version % 2 !== 0
           ? 
-          <RounteCmp
+          <RounteCmp2
             onClick={() => {
               this.props.dispatch({
                 type: 'ADD',
@@ -63,14 +62,7 @@ class App extends Component {
             version={this.props.version}
           />
           :
-          <div onClick={() => {
-            this.props.dispatch({
-              type: 'ADD',
-              value: DEFAULT.value,
-            });
-            DEFAULT.value += 3;
-            console.log(DEFAULT.value);
-          }}>No LoggedIn: {this.props.version}</div>
+          <NoLoggedInDom2 />
         }
       </Router>
     );
@@ -78,45 +70,73 @@ class App extends Component {
 }
 
 // testcase-1
-// const RounteCmp = ({onClick, version}) => {
-//   return (
-//     <div className="container">
-//       <nav className="navbar">
-//         <div className="navbar-brand">
-//           <a className="navbar-item"
-//              href="https://bulma.io">
-//             <img src="https://bulma.io/images/bulma-logo.png"
-//                  alt="Bulma: a modern CSS framework based on Flexbox"
-//                  width="112"
-//                  height="28" />
-//           </a>
-//           <div className="navbar-burger burger"
-//                data-target="navbarTop">
-//             <span></span>
-//             <span></span>
-//             <span></span>
-//           </div>
-//         </div>
-//         <div id="navbarTop"
-//              className="navbar-menu">
-//           <div className="navbar-start">
-//             <Link className="navbar-item"
-//                   to="/">ServerMonitorPage</Link>
-//             <Link className="navbar-item"
-//                   to="/ReleaseControlPage">ReleaseControlPage</Link>
-//             <div className="navbar-item"
-//                  onClick={onClick}>Version: {version}</div>
-//           </div>
-//         </div>
-//       </nav>
-//       <Route exact path="/" component={ServerMonitorPage} />
-//       <Route path="/ReleaseControlPage" component={ReleaseControlPage} />
-//     </div>
-//   )
-// }
+const RounteCmp = ({onClick, version, dispatch}) => {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a className="navbar-item"
+             href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png"
+                 alt="Bulma: a modern CSS framework based on Flexbox"
+                 width="112"
+                 height="28" />
+          </a>
+          <div className="navbar-burger burger"
+               data-target="navbarTop">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div id="navbarTop"
+             className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item"
+                  to="/">ServerMonitorPage</Link>
+            <Link className="navbar-item"
+                  to="/ReleaseControlPage">ReleaseControlPage</Link>
+            <div className="navbar-item"
+                 onClick={() => {
+                  dispatch({
+                    type: 'ADD',
+                    value: DEFAULT.value,
+                  });
+                  DEFAULT.value += 3;
+                  console.log(DEFAULT.value);
+                 }}>Version: {version}</div>
+          </div>
+        </div>
+      </nav>
+      <Route exact path="/" component={ServerMonitorPage} />
+      <Route path="/ReleaseControlPage" component={ReleaseControlPage} />
+    </div>
+  )
+}
 
-export default connect(state => {
+const RounteCmp2 = connect(state => {
   return {
     version: state.version,
   }
-})(App);
+})(RounteCmp);
+
+const NoLoggedInDom = ({version, dispatch}) => {
+  return (
+    <div onClick={() => {
+      dispatch({
+        type: 'ADD',
+        value: DEFAULT.value,
+      });
+      DEFAULT.value += 3;
+      console.log(DEFAULT.value);
+    }}>No LoggedIn: {version}</div>
+  )
+}
+
+const NoLoggedInDom2 = connect(state => {
+  return {
+    version: state.version,
+  }
+})(NoLoggedInDom);
+
+export default App;
