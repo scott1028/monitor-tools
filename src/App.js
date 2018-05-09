c9337c552eccae3737136313d23538a60d808933
scott <mic1028002@gmail.com>
2018-05-09 23:40:57 +0800

    update code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   Link
 } from 'react-router-dom'
 
-import RounteCmp from './RouteCmp';  // testcase-2
+// import RounteCmp from './RouteCmp';  // testcase-2
 
 const DEFAULT = {
   value: 1,
@@ -49,7 +49,8 @@ class App extends Component {
     return (
       <Router>
         {
-          this.props.version % 2 === 0
+          // this.props.version % 2 === 0
+          true
           ? 
           <RounteCmp
             onClick={() => {
@@ -78,42 +79,42 @@ class App extends Component {
 }
 
 // testcase-1
-// const RounteCmp = ({onClick, version}) => {
-//   return (
-//     <div className="container">
-//       <nav className="navbar">
-//         <div className="navbar-brand">
-//           <a className="navbar-item"
-//              href="https://bulma.io">
-//             <img src="https://bulma.io/images/bulma-logo.png"
-//                  alt="Bulma: a modern CSS framework based on Flexbox"
-//                  width="112"
-//                  height="28" />
-//           </a>
-//           <div className="navbar-burger burger"
-//                data-target="navbarTop">
-//             <span></span>
-//             <span></span>
-//             <span></span>
-//           </div>
-//         </div>
-//         <div id="navbarTop"
-//              className="navbar-menu">
-//           <div className="navbar-start">
-//             <Link className="navbar-item"
-//                   to="/">ServerMonitorPage</Link>
-//             <Link className="navbar-item"
-//                   to="/ReleaseControlPage">ReleaseControlPage</Link>
-//             <div className="navbar-item"
-//                  onClick={onClick}>Version: {version}</div>
-//           </div>
-//         </div>
-//       </nav>
-//       <Route exact path="/" component={ServerMonitorPage} />
-//       <Route path="/ReleaseControlPage" component={ReleaseControlPage} />
-//     </div>
-//   )
-// }
+const RounteCmp = ({onClick, version}) => {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a className="navbar-item"
+             href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png"
+                 alt="Bulma: a modern CSS framework based on Flexbox"
+                 width="112"
+                 height="28" />
+          </a>
+          <div className="navbar-burger burger"
+               data-target="navbarTop">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div id="navbarTop"
+             className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item"
+                  to="/">ServerMonitorPage</Link>
+            <Link className="navbar-item"
+                  to="/ReleaseControlPage">ReleaseControlPage</Link>
+            <div className="navbar-item"
+                 onClick={onClick}>Version: {version}</div>
+          </div>
+        </div>
+      </nav>
+      <Route exact path="/" component={ServerMonitorPage} />
+      <Route path="/ReleaseControlPage" component={ReleaseControlPage} />
+    </div>
+  )
+}
 
 export default connect(state => {
   return {
